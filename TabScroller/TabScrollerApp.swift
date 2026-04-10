d80af63e3baa7f6765bca74db278a0d6235e6470
Bertram <bertramqian@gmail.com>
2026-04-10 01:07:33 -0400

    Initial Commit

---
+++ TabScroller/TabScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabScrollerApp.swift
+//  TabScroller
+//
+//  Created by Bertram Qian on 2026-04-10.
+//
+
+import SwiftUI
+
+@main
+struct TabScrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
