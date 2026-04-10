22fb75b5e085ce935cfc8f22445d567692320772
Bertram <bertramqian@gmail.com>
2026-04-10 01:08:32 -0400

    Initial Commit

---
+++ GuitarTabScroller/GuitarTabScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarTabScrollerApp.swift
+//  GuitarTabScroller
+//
+//  Created by Bertram Qian on 2026-04-10.
+//
+
+import SwiftUI
+
+@main
+struct GuitarTabScrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
